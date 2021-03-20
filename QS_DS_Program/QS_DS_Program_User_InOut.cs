4f40a6fa227685c08d442973d1238ac1db7cae66
Kamoool <xkam555@gmail.com>
2021-03-20 18:08:00 +0100

    First implementation based on old version. This is not working here.Add project files.

---
+++ QS_DS_Program/QS_DS_Program_User_InOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QS_DS_Program
+{
+
+    public partial class QS_DS_Program : Form
+    {
+        private string IncomingData;
+        private void SerialDataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            IncomingData = MainSerialPort.ReadLine();
+            this.Invoke(new EventHandler(ReceivedDataHandle));
+        }
+
+
+
+
+    }
+}
